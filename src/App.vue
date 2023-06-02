2708939a18855f9bf5dd1f381d400033c5df1131
sha-255 <fawsgre@mail.ru>
2023-06-02 21:10:58 +0600

    Add saving to localStorage
    LocalStorage save added tickers

--- src/App.vue
+++ src/App.vue
@@ -157,7 +157,13 @@
           >
             <g>
               <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048 
+                c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z 
+                M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 
+                l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 
+                c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165 
+                c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0 
+                c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
                 fill="#718096"
                 data-original="#000000"
               ></path>
@@ -200,6 +206,7 @@ export default {
         this.intervals.find((i) => i.name === tickerToRemove.name).interval
       );
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
+      localStorage.setItem("capp-list", JSON.stringify(this.tickers));
     },
     onTextChanging() {
       this.errVisible = false;
@@ -222,26 +229,28 @@ export default {
         this.errVisible = true;
         return;
       }
-      if (!tickerName) {
-        return;
-      }
+      if (!tickerName) return;
       const currentTicker = {
         name: tickerName,
         price: "-"
       };
       this.tickers.push(currentTicker);
+      localStorage.setItem("capp-list", JSON.stringify(this.tickers));
+      this.subscribeByUpdate(currentTicker.name);
+    },
+    subscribeByUpdate(tickerName) {
       this.intervals.push({
-        name: currentTicker.name,
+        name: tickerName,
         interval: setInterval(async () => {
           const f = await fetch(
             `https://min-api.cryptocompare.com` +
-              `/data/price?fsym=${currentTicker.name}&tsyms=USD&` +
+              `/data/price?fsym=${tickerName}&tsyms=USD&` +
               `api_key=4302c479b9cec17604db7af005cb60a54b9d1079a8b58fd18ba964bc6d16826d`
           );
           const data = await f.json();
-          this.tickers.find((t) => t.name === currentTicker.name).price =
+          this.tickers.find((t) => t.name === tickerName).price =
             data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-          if (this.sel?.name === currentTicker.name) {
+          if (this.sel?.name === tickerName) {
             this.graph.push(data.USD);
           }
         }, 10_000)
@@ -281,6 +290,13 @@ export default {
     const tmp = await data();
     this.coinsNames = Object.keys(tmp.Data);
     this.isStarted = false;
+  },
+  created() {
+    const ticersData = localStorage.getItem("capp-list");
+    if (ticersData) {
+      this.tickers = JSON.parse(ticersData);
+      this.tickers.forEach((ticker) => this.subscribeByUpdate(ticker.name));
+    }
   }
 };
 </script>

6966194defbaa00cf6ee792480cd19844417961a
sha-255 <fawsgre@mail.ru>
2023-07-05 17:59:33 +0600

    pre TickersGraph component

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,7 @@
   <div
     class="container mx-auto flex flex-col items-center bg-gray-100 p-4 rounded-lg"
   >
-    <div
-      v-if="isStarted"
-      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <loading-bar :isVisible="isLoading" />
     <div class="container">
       <add-ticker @add-ticker="handleAdd" :disabled="tooManyTickersAdded" />
       <template v-if="tickers.length > 0">
@@ -92,25 +68,15 @@
               @click.stop="handleDelete(t)"
               class="flex items-start justify-center font-medium w-full h-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <trash-svg />
               Удалить
             </button>
           </div>
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
+      <!-- graph -->
+      <!-- v-if="selectedTicker && selectedTicker.price !== undefinedPriceText" -->
       <section
         v-if="selectedTicker && selectedTicker.price !== undefinedPriceText"
         class="relative"
@@ -137,35 +103,10 @@
           type="button"
           class="absolute top-0 right-0"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048 
-                c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z 
-                M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 
-                l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251 
-                c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165 
-                c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0 
-                c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
+          <cross-svg />
         </button>
       </section>
+      <!---->
     </div>
   </div>
 </template>
@@ -173,10 +114,16 @@
 <script type="module">
 import { getCoinsNames, subscribeToTicker, unsubscribeFromTicker } from "./api";
 import AddTicker from "./components/AddTicker.vue";
+import CrossSvg from "./components/CrossSvg.vue";
+import TrashSvg from "./components/TrashSvg.vue";
+import LoadingBar from "./components/LoadingBar.vue";
 
 export default {
   components: {
-    AddTicker
+    AddTicker,
+    CrossSvg,
+    TrashSvg,
+    LoadingBar
   },
   data() {
     return {
@@ -196,11 +143,12 @@ export default {
     };
   },
   methods: {
+    //Graph
     calculateMaxGraphElements() {
       if (!this.$refs.graph) return;
       this.maxGraphElements = this.$refs.graph.clientWidth / this.barWidth; //this.$refs.bar[0]?.clientWidth
-      console.log("!", this.maxGraphElements);
     },
+    ///
     updateTicker(tickerName, price) {
       this.tickers
         .filter((t) => t.name === tickerName)
@@ -312,6 +260,7 @@ export default {
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex;
     },
+    //Graph
     normalizedGraph() {
       const maxValue = Math.max(...this.graph);
       const minValue = Math.min(...this.graph);
@@ -320,6 +269,7 @@ export default {
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
     },
+    //
     pageStateOptions() {
       return {
         filter: this.filter,
---
+++ src/components/TickersGraph.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="relative">
+    <h3 class="text-lg leading-6 font-medium text-gray-900 my-8" ref="graph">
+      {{ selectedTicker.name }} - USD
+    </h3>
+    <div
+      class="flex items-end border-gray-600 border-b-4 border-l-4 h-64 rounded-lg"
+    >
+      <div
+        v-for="(bar, idx) in normalizedGraph"
+        :key="idx"
+        :style="{ height: `${bar}%`, width: `${barWidth}px` }"
+        class="bg-transparent border-4 border-purple-800 rounded-lg"
+      ></div>
+    </div>
+    <button
+      @click="$emit('graph-close')"
+      type="button"
+      class="absolute top-0 right-0"
+    >
+      <cross-svg />
+    </button>
+  </section>
+</template>
+<script>
+import CrossSvg from "./CrossSvg.vue";
+
+export default {
+  components: {
+    CrossSvg
+  },
+  props: {
+    selectedTicker: {
+      reqired: false,
+      default: null
+    },
+    barWidth: {
+      type: Number,
+      reqired: false,
+      default: 32
+    }
+  },
+  emits: {
+    "graph-close": null
+  },
+  methods: {
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) return;
+      this.maxGraphElements = this.$refs.graph.clientWidth / this.barWidth;
+    }
+  },
+  computed: {
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+      if (maxValue === minValue) return this.graph.map(() => 50);
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    }
+  }
+};
+</script>
